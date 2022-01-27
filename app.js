fcabad5e80ab7cd471ea4f854e36719a257cd92a
SignMarin <marin.lebrec@outlook.fr>
2022-01-27 16:30:12 +0100

    page travel fini

--- app.js
+++ app.js
@@ -1,52 +1,65 @@
 require('./models/connection')
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
 
-var loginRouter = require('./routes/login');
-var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login')
+var usersRouter = require('./routes/users')
 var homeRouter = require('./routes/home')
-var session = require("express-session");
+var session = require('express-session')
 
-var app = express();
+var app = express()
 app.use(
   session({
-   secret: 'a4f8071f-c873-4447-8ee2',
-   resave: false,
-   saveUninitialized: false,
+    secret: 'a4f8071f-c873-4447-8ee2',
+    resave: false,
+    saveUninitialized: false,
   })
-  );
+)
+
+app.locals.parseDate = function (date) {
+  var day = String(date.getDate())
+  if (day.length < 2) {
+    day = `0${day}`
+  }
+  var month = String(date.getMonth() + 1)
+  if (month.length < 2) {
+    month = `0${month}`
+  }
+
+  return `${day}/${month}`
+}
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', loginRouter);
-app.use('/users', usersRouter);
-app.use('/home', homeRouter);
+app.use('/', loginRouter)
+app.use('/users', usersRouter)
+app.use('/home', homeRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(function (req, res, next) {
+  next(createError(404))
+})
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-module.exports = app;
+module.exports = app
--- routes/home.js
+++ routes/home.js
@@ -1,27 +1,36 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 var journeyModel = require('../models/journeys')
 var userModel = require('../models/users')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  if(req.session.user === undefined){
-    res.render('login')
+router.get('/', function (req, res, next) {
+  if (req.session.user === undefined) {
+    res.redirect('/')
   } else {
     res.render('index')
-  };
-});
+  }
+})
 
-router.post('/travel', async function(req, res, next){
+router.get('/travel', function (req, res, next) {
+  if (req.session.user === undefined) {
+    res.redirect('/')
+  } else {
+    var journeys = []
+    res.render('results', { journeys })
+  }
+})
+
+router.post('/travel', async function (req, res, next) {
   var journeys = await journeyModel.find({
     departure: req.body.depart,
     arrival: req.body.arrivée,
-    date: req.body.date
-  });
+    date: req.body.date,
+  })
   console.log(journeys)
-    res.render('results', {journeys})
-});
+  res.render('results', { journeys })
+})
 
-module.exports = router;
+module.exports = router
