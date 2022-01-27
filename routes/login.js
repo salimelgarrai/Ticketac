d0cd2070e4c92f95f25394773a9851b86a1da38a
SignMarin <marin.lebrec@outlook.fr>
2022-01-27 13:22:52 +0100

    a table !!

--- routes/login.js
+++ routes/login.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 var journeyModel = require('../models/journeys')
 var userModel = require('../models/users')
 
 /* GET login page. */
-router.get('/', function(req, res, next) {
-  res.render('login');
-});
+router.get('/', function (req, res, next) {
+  res.render('login')
+})
 
-module.exports = router;
+module.exports = router
