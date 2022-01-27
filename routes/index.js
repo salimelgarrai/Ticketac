6027e70602af0b6712d958018651d3d0c85217ba
SignMarin <marin.lebrec@outlook.fr>
2022-01-27 11:51:46 +0100

    login front

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 var journeyModel = require('../models/journeys')
 var userModel = require('../models/users')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
+router.get('/', function (req, res, next) {
+  res.render('login')
+})
 
-module.exports = router;
+module.exports = router
