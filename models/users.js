aa61c78d9005b4e3fafbe9f8d6a03bc9e7c90363
Salim Elgarrai <salim.elgarrai@gmail.com>
2022-01-27 16:42:54 +0100

    my trips

--- models/users.js
+++ models/users.js
@@ -1,10 +1,18 @@
 var mongoose = require('mongoose')
 
+var lastSchema = mongoose.Schema({
+    date: Date,
+    journey: String,
+    departureTime: String,
+    price: Number,
+})
+
 var userSchema = mongoose.Schema({
     name: String,
     firstName: String,
     email:  String,
     password: String,
+    lastTrip: [{lastSchema}]
 })
 
 module.exports = mongoose.model('users', userSchema)
