d5c4931bba4810b8f700f97953ceb9de261ae364
Salim Elgarrai <salim.elgarrai@gmail.com>
2022-01-27 10:33:06 +0100

    Début du projet

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+const mongoose = require('mongoose');
+var journeyModel = require('../models/journeys')
+
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/sing-in', async function(req, res, next) {
+  res.send('index');
 });
+
+router.get('/sign-up', async function(req, res, next){
+  res.send('index')
+})
 
 module.exports = router;
